52b9840d6115f77ad14232db5077ec9ae1c88bd9
Dmitry Khalansky <rouanth@gmail.com>
2016-10-16 13:26:46 +0300

    Abstracted filtering the surrounding branches
    Added function `filter_around` which accepts a field, a description of
    how to treat its boundaries, the required state, and a point around
    which to perform filtering, and for each surrounding cell that's
    eligible calls the passed function.
    
    Rewritten empty_neighbors using the new function.
    
    Also `true` now represents the wall, and `false` represents the floor.

--- src/labyrinth.rs
+++ src/labyrinth.rs
@@ -14,6 +14,9 @@ enum UpDown {
     Down = 1,
 }
 
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+struct Direction((LeftRight, UpDown));
+
 pub struct Labyrinth {
     cells: Vec<Vec<bool>>
 }
@@ -23,7 +26,7 @@ impl fmt::Debug for Labyrinth {
         let mut v = Vec::new();
         for i in self.cells.iter() {
             let s : String = i.iter().map(
-                |&y| if y {' '} else {'#'}).collect();
+                |&y| if y {'#'} else {' '}).collect();
             v.push(s);
         }
         write!(f, "{}", v.join("\n"))
@@ -35,7 +38,7 @@ impl fmt::Display for Labyrinth {
         for i in 0..self.cells.len() {
             let len = self.cells[i].len();
             for j in 0..len {
-                try!(write!(f, "{}", if self.cells[i][j] {' '} else {'#'}));
+                try!(write!(f, "{}", if self.cells[i][j] {'#'} else {' '}));
             }
             if i != len - 1 {
                 try!(write!(f, "\n"));
@@ -52,22 +55,28 @@ impl Labyrinth {
         Labyrinth{cells : v}
     }
 
-    pub fn empty_neighbors(&self, i: usize, j: usize)
-        -> Vec<(LeftRight, UpDown)> {
-            let mut res = Vec::new();
-            let e = vec![true; self.cells[i].len()];
+    fn filter_around<T, F>(c: &Vec<Vec<bool>>, def: bool, req: bool, p: &Point,
+                        t: &mut T, mut f: F)
+        where F: FnMut(&mut T, usize, usize, UpDown, LeftRight) {
+            let Point{x, y} = *p;
+            let e = vec![def; c[x].len()];
             let lr = [LeftRight::Left, LeftRight::Middle, LeftRight::Right];
             let ud = [UpDown::Up, UpDown::Middle, UpDown::Down];
             for h in lr.iter() { for v in ud.iter() {
-                let hidx = (j as i32 + *h as i32) as usize;
-                let vidx = (i as i32 + *v as i32) as usize;
-                if *self.cells.
-                    get(hidx).unwrap_or(&e).get(vidx).unwrap_or(&true) {
-                        res.push((*h, *v));
-                    }
+                let hidx = (y as i32 + *h as i32) as usize;
+                let vidx = (x as i32 + *v as i32) as usize;
+                if !req^*c.get(vidx).unwrap_or(&e).get(hidx).unwrap_or(&def) {
+                    f(t, vidx, hidx, *v, *h);
+                }
             } }
-            res
         }
+
+    fn empty_neighbors(&self, p: &Point) -> Vec<Direction> {
+        let mut res = Vec::new();
+        Labyrinth::filter_around(&self.cells, false, false, p, &mut res,
+                                 |m, _, _, v, h| m.push(Direction((h, v))));
+        res
+    }
 }
 
 pub struct Point {
