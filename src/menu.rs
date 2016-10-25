9077be3e11f744744648e286bdd86b98e7acfbac
Dmitry Khalansky <rouanth@gmail.com>
2016-10-25 23:26:56 +0300

    Specified the Screen trait
    Screen views are among the most important structures of the game. The
    Screen trait is a Controller and Screen instances are Models in terms of
    MVC paradigm. Once all signals to screens are described and have a
    well-defined meaning, it's a matter of simple wrapping to create views
    for them.

--- src/menu.rs
+++ src/menu.rs
@@ -1,18 +1,29 @@
 use screen;
+use state;
 
-#[derive(Debug)]
-pub struct MenuScreen {
+pub struct MenuScreen<'b> {
+    state: &'b mut state::ProgramState,
 }
 
-impl screen::Screen for MenuScreen {
-
-}
-
-impl MenuScreen {
-    pub fn new() -> MenuScreen {
-        MenuScreen {
-
+impl<'b> screen::Screen for MenuScreen<'b> {
+    fn tick<'a>(&'a mut self, input: Option<screen::UserInput>) ->
+        &'a mut screen::Screen {
+            if let Some(i) = input {
+                match i {
+                    UserInput::Accept => (),
+                    UserInput::Cancel => (),
+                    UserInput::Direction(d) => (),
+                    UserInput::Menu => (),
+                }
+            }
+            self
         }
-    }
+
+    fn new<'a>(state: &'a mut state::ProgramState)
+        -> Box<screen::Screen + 'a> {
+            Box::new(MenuScreen {
+                state: state,
+            })
+        }
 }
 
--- src/screen.rs
+++ src/screen.rs
@@ -1,5 +1,16 @@
-use std::fmt;
+use direction;
+use state;
 
-pub trait Screen: fmt::Debug {
+pub enum UserInput {
+    Accept,
+    Cancel,
+    Direction(direction::Direction),
+    Menu,
+}
+
+pub trait Screen {
+    fn tick<'a>(&'a mut self, Option<UserInput>) -> &'a mut Screen;
+    fn new<'a>(state: &'a mut state::ProgramState) -> Box<Screen + 'a>
+        where Self: Sized;
 }
 
