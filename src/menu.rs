0df1c5f75813862b0e5b4b0fdda881a7bcd1587b
Dmitry Khalansky <rouanth@gmail.com>
2017-01-04 17:33:22 +0300

    Navigate high scores and quit dialog
    Even though the game doesn't actually quit yet, we have working
    mechanisms for navigating the corresponding menu.

--- src/menu.rs
+++ src/menu.rs
@@ -36,13 +36,17 @@ const OPTION_ITEMS: [Options; 3] = [
 
 impl Options {
     fn next(&self) -> Options {
-        OPTION_ITEMS[OPTION_ITEMS.iter().position(
-            |x| self.eq(x)).unwrap_or(0)]
+        OPTION_ITEMS[(self.position().unwrap_or(0) + 1)
+            % OPTION_ITEMS.len()]
     }
 
     fn prev(&self) -> Options {
-        OPTION_ITEMS[OPTION_ITEMS.iter().rposition(
-            |x| self.eq(x)).unwrap_or(0)]
+        OPTION_ITEMS[(self.position().unwrap_or(0) + OPTION_ITEMS.len() - 1)
+            % OPTION_ITEMS.len()]
+    }
+
+    fn position(&self) -> Option<usize> {
+        OPTION_ITEMS.iter().position(|x| self.eq(x))
     }
 }
 
@@ -97,7 +101,14 @@ impl Screen for MenuScreen {
                     self.tick_license(i, line);
                     None
                 },
-                _ => None
+                Subscreens::HighScore => {
+                    self.tick_highscore(i);
+                    None
+                },
+                Subscreens::Quit(m, q) => {
+                    self.tick_quit(i, m, q);
+                    None
+                }
             }
         } else {
             None
@@ -211,6 +222,35 @@ impl MenuScreen {
             },
             Input::Cancel => {
                 self.subscreen = Subscreens::Menu(Menu::License);
+            },
+            _ => ()
+        }
+    }
+
+    fn tick_quit(&mut self, input: Input, menu: Menu, quit: bool) {
+        match input {
+            Input::Direction(_) => {
+                self.subscreen = Subscreens::Quit(menu, !quit);
+            },
+            Input::Cancel => {
+                self.subscreen = Subscreens::Menu(menu);
+            },
+            Input::Accept => {
+                if quit {
+                    self.subscreen = Subscreens::Menu(menu);
+                    // TODO: Actually quit.
+                } else {
+                    self.subscreen = Subscreens::Menu(menu);
+                }
+            },
+            _ => ()
+        }
+    }
+
+    fn tick_highscore(&mut self, input: Input) {
+        match input {
+            Input::Cancel => {
+                self.subscreen = Subscreens::Menu(Menu::HighScore);
             },
             _ => ()
         }
