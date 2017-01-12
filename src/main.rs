09329c539fee1fd13d4d0be525fbae47739da8f8
Dmitry Khalansky <rouanth@gmail.com>
2017-01-12 12:05:04 +0300

    Accepting events from glutin window
    We don't write anything to the glutin window yet but we use it to
    receive key presses. It allows us to advance the state of the game.
    However, it still is displayed as debug output in console.

--- src/main.rs
+++ src/main.rs
@@ -10,32 +10,98 @@ mod screen;
 mod geometry;
 mod render;
 
+#[macro_use]
+extern crate glium;
+
+use std::io::Cursor;
+use std::time::{Duration, Instant};
+use std::thread;
+
+use glium::{DisplayBuild, Surface};
+use glium::glutin;
+use glium::index::PrimitiveType;
+
+use glium::glutin::VirtualKeyCode;
+
 use tickable::Input;
 use screen::Screen;
 use direction::{DIR_UP,DIR_DOWN,DIR_LEFT,DIR_RIGHT};
 use std::io::{Read,stdin};
+use geometry::Worldly;
+
+enum Action {
+    Continue,
+    Key(glutin::VirtualKeyCode),
+    Stop
+}
+
+// Taken from glium.git/examples/support/mod.rs
+pub fn start_loop<F>(scr: &mut Box<Screen>, mut callback: F)
+    where F: FnMut() -> Action {
+        let mut accumulator = Duration::new(0, 0);
+        let mut previous_clock = Instant::now();
+
+        loop {
+            let mut key = None;
+            match callback() {
+                Action::Stop => break,
+                Action::Key(k) => key = Some(k),
+                Action::Continue => ()
+            };
+
+            let now = Instant::now();
+            accumulator += now - previous_clock;
+            previous_clock = now;
+
+            let fixed_time_stamp = Duration::new(0, 16666667);
+            while accumulator >= fixed_time_stamp {
+                accumulator -= fixed_time_stamp;
+
+                // Modifying the state of the game
+                if let Some(b) = key {
+                    let ev = match b {
+                        VirtualKeyCode::H => Some(Input::Direction(DIR_LEFT)),
+                        VirtualKeyCode::J => Some(Input::Direction(DIR_DOWN)),
+                        VirtualKeyCode::K => Some(Input::Direction(DIR_UP)),
+                        VirtualKeyCode::L => Some(Input::Direction(DIR_RIGHT)),
+                        VirtualKeyCode::Z => Some(Input::Accept),
+                        VirtualKeyCode::X => Some(Input::Cancel),
+                        VirtualKeyCode::Colon => Some(Input::Menu),
+                        _ => None
+                    };
+                    if let Some(f) = scr.tick(ev) {
+                        *scr = f;
+                    }
+                    println!("{:?}", scr);
+                }
+            }
+
+            thread::sleep(fixed_time_stamp - accumulator);
+        }
+}
 
 fn main() {
+    let display = glutin::WindowBuilder::new()
+        .with_vsync()
+        .build_glium()
+        .unwrap();
+
     let pr = state::ProgramState::new();
     let mut scr : Box<Screen> = Box::new(menu::MenuScreen::new(pr));
-    for b in stdin().bytes() {
-        let c = b.unwrap_or(0) as char;
-        if c != '\n' && c != '\0' {
-            let s = match c {
-                'h' => Some(Input::Direction(DIR_LEFT)),
-                'j' => Some(Input::Direction(DIR_DOWN)),
-                'k' => Some(Input::Direction(DIR_UP)),
-                'l' => Some(Input::Direction(DIR_RIGHT)),
-                'z' => Some(Input::Accept),
-                'x' => Some(Input::Cancel),
-                ':' => Some(Input::Menu),
-                _   => None
-            };
-            if let Some(f) = scr.tick(s) {
-                scr = f;
+    start_loop(&mut scr, || {
+        for event in display.poll_events() {
+            match event {
+                glutin::Event::Closed => return Action::Stop,
+                glutin::Event::KeyboardInput(
+                    glutin::ElementState::Pressed,
+                    _,
+                    Some(key)
+                    ) => return Action::Key(key),
+                _ => ()
             }
-            println!("{:?}", scr);
         }
-    }
+        Action::Continue
+    });
+
 }
 
