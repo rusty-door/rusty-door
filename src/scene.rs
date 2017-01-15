273ec3f5a3473eb56a7e83cb524842b855917496
Dmitry Khalansky <rouanth@gmail.com>
2017-01-15 18:25:55 +0300

    Implement a basic scene
    Two parallelograms on a plane.

--- src/scene.rs
+++ src/scene.rs
@@ -14,8 +14,112 @@ pub fn scene() -> World {
             color: ColorGenerator::Uniform(RGB(0x66, 0xFF, 0x30)),
         }
     };
+    let cube1 = Shape {
+        primitive : Primitive::TriangleList,
+        verts: vec!(
+            Vector3(150.0,  50.1, 0.5),
+            Vector3(150.0,  50.1, 0.8),
+            Vector3(250.0,  50.1, 0.5),
+            Vector3(150.0,  50.1, 0.8),
+            Vector3(250.0,  50.1, 0.5),
+            Vector3(250.0,  50.1, 0.8),
+
+            Vector3(250.0,  50.1, 0.5),
+            Vector3(250.0, 150.1, 0.5),
+            Vector3(250.0,  50.1, 0.8),
+            Vector3(250.0, 150.1, 0.5),
+            Vector3(250.0,  50.1, 0.8),
+            Vector3(250.0, 150.1, 0.8),
+
+            Vector3(150.0, 150.1, 0.5),
+            Vector3(150.0, 150.1, 0.8),
+            Vector3(250.0, 150.1, 0.5),
+            Vector3(150.0, 150.1, 0.8),
+            Vector3(250.0, 150.1, 0.5),
+            Vector3(250.0, 150.1, 0.8),
+
+            Vector3(150.0,  50.1, 0.5),
+            Vector3(150.0, 150.1, 0.5),
+            Vector3(150.0,  50.1, 0.8),
+            Vector3(150.0, 150.1, 0.5),
+            Vector3(150.0,  50.1, 0.8),
+            Vector3(150.0, 150.1, 0.8),
+
+            Vector3(150.0,  50.1, 0.5),
+            Vector3(150.0, 150.1, 0.5),
+            Vector3(250.0,  50.1, 0.5),
+            Vector3(150.0, 150.1, 0.5),
+            Vector3(250.0,  50.1, 0.5),
+            Vector3(250.0, 150.1, 0.5),
+
+            Vector3(150.0,  50.1, 0.8),
+            Vector3(150.0, 150.1, 0.8),
+            Vector3(250.0,  50.1, 0.8),
+            Vector3(150.0, 150.1, 0.8),
+            Vector3(250.0,  50.1, 0.8),
+            Vector3(250.0, 150.1, 0.8),
+        ),
+        material: Material {
+            color: ColorGenerator::Linear(
+                RGB(0x66, 0x32, 0x00),
+                RGB(0x34, 0x00, 0x22),
+                RGB(0xFF, 0x66, 0x00))
+        }
+    };
+    let cube2 = Shape {
+        primitive : Primitive::TriangleList,
+        verts: vec!(
+            Vector3(400.0,  50.1, 0.5),
+            Vector3(400.0,  50.1, 0.8),
+            Vector3(550.0,  50.1, 0.5),
+            Vector3(400.0,  50.1, 0.8),
+            Vector3(550.0,  50.1, 0.5),
+            Vector3(550.0,  50.1, 0.8),
+
+            Vector3(550.0,  50.1, 0.5),
+            Vector3(550.0, 400.1, 0.5),
+            Vector3(550.0,  50.1, 0.8),
+            Vector3(550.0, 400.1, 0.5),
+            Vector3(550.0,  50.1, 0.8),
+            Vector3(550.0, 400.1, 0.8),
+
+            Vector3(400.0, 400.1, 0.5),
+            Vector3(400.0, 400.1, 0.8),
+            Vector3(550.0, 400.1, 0.5),
+            Vector3(400.0, 400.1, 0.8),
+            Vector3(550.0, 400.1, 0.5),
+            Vector3(550.0, 400.1, 0.8),
+
+            Vector3(400.0,  50.1, 0.5),
+            Vector3(400.0, 400.1, 0.5),
+            Vector3(400.0,  50.1, 0.8),
+            Vector3(400.0, 400.1, 0.5),
+            Vector3(400.0,  50.1, 0.8),
+            Vector3(400.0, 400.1, 0.8),
+
+            Vector3(400.0,  50.1, 0.5),
+            Vector3(400.0, 400.1, 0.5),
+            Vector3(550.0,  50.1, 0.5),
+            Vector3(400.0, 400.1, 0.5),
+            Vector3(550.0,  50.1, 0.5),
+            Vector3(550.0, 400.1, 0.5),
+
+            Vector3(400.0,  50.1, 0.8),
+            Vector3(400.0, 400.1, 0.8),
+            Vector3(550.0,  50.1, 0.8),
+            Vector3(400.0, 400.1, 0.8),
+            Vector3(550.0,  50.1, 0.8),
+            Vector3(550.0, 400.1, 0.8),
+        ),
+        material: Material {
+            color: ColorGenerator::Linear(
+                RGB(0x00, 0xFF, 0x00),
+                RGB(0xFF, 0x00, 0xFF),
+                RGB(0xFF, 0x00, 0xFF))
+        }
+    };
     World {
-        shapes: vec!(floor),
+        shapes: vec!(floor, cube1, cube2),
         lighting: vec!(light)
     }
 }
