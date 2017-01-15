a28babb6e225caa83d6912a3dc3bc23e2c508bba
Dmitry Khalansky <rouanth@gmail.com>
2017-01-15 12:59:38 +0300

    Add walls
    Their implementation can certainly be more efficient, but it's better
    than nothing.

--- src/play.rs
+++ src/play.rs
@@ -29,57 +29,118 @@ impl Worldly for PlayScreen {
             light.push(Vector3(game.player.x as i32, game.player.y as i32,
                               1).into_inner());
 
+            let zf = 0.3;
+            let zn = 0.26;
+
             let w = game.field.0.width() as i32;
             let h = game.field.0.height() as i32;
 
             for x in 0 .. h {
                 for y in 0 .. w {
-                    let coord_to_vertex = |r: RGB, c: &Vector3<i32>| Vertex {
-                                       coords: c.into_inner(),
+                    let coord_to_vertex = |r: RGB, c: &Vector3<f64>| Vertex {
+                                       coords: *c,
                                        color: r
                     };
                     if game.field.0[Point{x: x as usize, y: y as usize}] {
                         let r = RGB(0x61, 0x40, 0x20);
                         shapes.push(Shape {
                             verts: vec!(
-                             Vector3(( y  ) * 640 / w, (x+1) * 480 / h, 2),
-                             Vector3(( y+1) * 640 / w, (x+1) * 480 / h, 2),
-                             Vector3(( y  ) * 640 / w, (x  ) * 480 / h, 2),
-                             Vector3(( y+1) * 640 / w, (x  ) * 480 / h, 2)).
+                             Vector3((( y  ) * 640 / w) as f64, ((x+1) * 480 / h) as f64, zn),
+                             Vector3((( y+1) * 640 / w) as f64, ((x+1) * 480 / h) as f64, zn),
+                             Vector3((( y  ) * 640 / w) as f64, ((x  ) * 480 / h) as f64, zn),
+                             Vector3((( y+1) * 640 / w) as f64, ((x  ) * 480 / h) as f64, zn)).
                                      iter().map(|c| coord_to_vertex(r, c)).
                                      collect(),
                             primitive: Primitive::TriangleStrip,
                         });
+
+                        let m = RGB(0x33, 0x41, 0x33);
+                        if y > 0 && !*game.field.0.get(Point{x:x as usize, y: (y-1) as usize}).unwrap_or(&false) {
+                            shapes.push(Shape {
+                                verts: vec!(
+                                 Vector3((( y  ) * 640 / w) as f64, ((x+1) * 480 / h) as f64, zn),
+                                 Vector3((( y  ) * 640 / w) as f64, ((x  ) * 480 / h) as f64, zn),
+                                 Vector3((( y  ) * 640 / w) as f64, ((x+1) * 480 / h) as f64, zf),
+                                 Vector3((( y  ) * 640 / w) as f64, ((x  ) * 480 / h) as f64, zf)).
+                                         iter().map(|c| coord_to_vertex(m, c)).
+                                         collect(),
+                                primitive: Primitive::TriangleStrip,
+                            });
+                        }
+                        let n = RGB(0x41, 0x33, 0x41);
+                        if !*game.field.0.get(Point{x:x as usize, y: (y+1) as usize}).unwrap_or(&false) {
+                            shapes.push(Shape {
+                                verts: vec!(
+                                 Vector3((( y+1 ) * 640 / w) as f64, ((x+1) * 480 / h) as f64, zn),
+                                 Vector3((( y+1 ) * 640 / w) as f64, ((x  ) * 480 / h) as f64, zn),
+                                 Vector3((( y+1 ) * 640 / w) as f64, ((x+1) * 480 / h) as f64, zf),
+                                 Vector3((( y+1 ) * 640 / w) as f64, ((x  ) * 480 / h) as f64, zf)).
+                                         iter().map(|c| coord_to_vertex(n, c)).
+                                         collect(),
+                                primitive: Primitive::TriangleStrip,
+                            });
+                        }
+                        let o = RGB(0x71, 0x15, 0xAA);
+                        if x > 0 && !*game.field.0.get(Point{x:(x-1) as usize, y: y as usize}).unwrap_or(&false) {
+                            shapes.push(Shape {
+                                verts: vec!(
+                                 Vector3((( y+1 ) * 640 / w) as f64, ((x  ) * 480 / h) as f64, zn),
+                                 Vector3((( y   ) * 640 / w) as f64, ((x  ) * 480 / h) as f64, zn),
+                                 Vector3((( y+1 ) * 640 / w) as f64, ((x  ) * 480 / h) as f64, zf),
+                                 Vector3((( y   ) * 640 / w) as f64, ((x  ) * 480 / h) as f64, zf)).
+                                         iter().map(|c| coord_to_vertex(o, c)).
+                                         collect(),
+                                primitive: Primitive::TriangleStrip,
+                            });
+                        }
+                        let p = RGB(0x33, 0xBB, 0x33);
+                        if !*game.field.0.get(Point{x:(x+1) as usize, y: y as usize}).unwrap_or(&false) {
+                            shapes.push(Shape {
+                                verts: vec!(
+                                 Vector3((( y+1 ) * 640 / w) as f64, ((x+1) * 480 / h) as f64, zn),
+                                 Vector3((( y   ) * 640 / w) as f64, ((x+1) * 480 / h) as f64, zn),
+                                 Vector3((( y+1 ) * 640 / w) as f64, ((x+1) * 480 / h) as f64, zf),
+                                 Vector3((( y   ) * 640 / w) as f64, ((x+1) * 480 / h) as f64, zf)).
+                                         iter().map(|c| coord_to_vertex(p, c)).
+                                         collect(),
+                                primitive: Primitive::TriangleStrip,
+                            });
+                        }
                     } else {
                         let r = RGB(0x40, 0x20, 0x61);
                         shapes.push(Shape {
                             verts: vec!(
-                             Vector3(( y  ) * 640 / w, (x+1) * 480 / h, 2),
-                             Vector3(( y+1) * 640 / w, (x+1) * 480 / h, 2),
-                             Vector3(( y  ) * 640 / w, (x  ) * 480 / h, 2),
-                             Vector3(( y+1) * 640 / w, (x  ) * 480 / h, 2)).
+                             Vector3((( y  ) * 640 / w) as f64, ((x+1) * 480 / h) as f64, zf),
+                             Vector3((( y+1) * 640 / w) as f64, ((x+1) * 480 / h) as f64, zf),
+                             Vector3((( y  ) * 640 / w) as f64, ((x  ) * 480 / h) as f64, zf),
+                             Vector3((( y+1) * 640 / w) as f64, ((x  ) * 480 / h) as f64, zf)).
                                      iter().map(|c| coord_to_vertex(r, c)).
                                      collect(),
                             primitive: Primitive::TriangleStrip,
                         });
                     }
+
+                    let player = Shape {
+                        verts: vec!(
+                            Vertex {
+                                color: RGB(0x00, 0xFF, 0xFF),
+                                coords: Vector3((game.player.y as i32 * 640 / w) as f64, (game.player.x as i32 * 480 / h) as f64, (zf + zn) / 2.0)
+                            },
+                            Vertex {
+                                color: RGB(0xFF, 0x00, 0xFF),
+                                coords: Vector3((game.player.y as i32 * 640 / w) as f64, ((game.player.x as i32 + 1) * 480 / h) as f64, (zf + zn) / 2.0)
+                            },
+                            Vertex {
+                                color: RGB(0xFF, 0xFF, 0x00),
+                                coords: Vector3(((game.player.y as i32 + 1) * 640 / w) as f64, (game.player.x as i32 * 480 / h) as f64, (zf + zn) / 2.0)
+                            }),
+                        primitive: Primitive::TriangleStrip
+                    };
+
+                    shapes.push(player);
                 }
             }
         }
-        let coord_to_vertex = |r: RGB, c: &Vector3<f64>| Vertex {
-                           coords: *c,
-                           color: r
-        };
-        shapes.push(Shape {
-                   verts: vec!(
-                       Vector3(0.1  , 479.0, 3.1),
-                       Vector3(0.1  , 0.1  , 3.1),
-                       Vector3(639.0, 479.0, 3.1),
-                       Vector3(639.0, 0.1  , 3.1)).iter().map(
-                           |c| coord_to_vertex(RGB(0x15, 0x15, 0x15), c)).
-                           collect(),
-                   primitive: Primitive::TriangleStrip,
-        });
 
         World {
             shapes   : shapes,
