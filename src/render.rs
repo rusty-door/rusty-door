6cb3dbdcc8dd18b8d0117fb228af8ee9aa339cca
Dmitry Khalansky <rouanth@gmail.com>
2017-01-15 18:36:27 +0300

    No need for space partitioning
    For the renderer, space partitioning only gets in the way.

--- src/render.rs
+++ src/render.rs
@@ -10,156 +10,6 @@ pub struct Canvas {
     zbuffer: Vec<Vec<f64>>,
 }
 
-struct Voxel<'b> {
-    polygons: Vec<&'b Polygon>
-}
-
-impl<'b> Default for Voxel<'b> {
-    fn default() -> Voxel<'b> {
-        Voxel {
-            polygons: vec!()
-        }
-    }
-}
-
-struct Space<'b> {
-    grid: [[[Voxel<'b>; 12]; 12]; 12]
-}
-
-impl<'b> Space<'b> {
-    fn new(poly: &'b Vec<Polygon>) -> Space<'b> {
-        let mut g : [[[Voxel; 12]; 12]; 12] = Default::default();
-        for p in poly {
-            let coords = [p.coords.0, p.coords.1, p.coords.2];
-
-            let cmp = |x: &f64, y: &f64| -> Ordering {
-                if *x < *y {
-                    Ordering::Less
-                } else {
-                    Ordering::Greater
-                }
-            };
-
-            let mut xs : Vec<f64> = coords.iter().map(|c| c.0).collect();
-            xs.sort_by(&cmp);
-            let mut ys : Vec<f64> = coords.iter().map(|c| c.1).collect();
-            ys.sort_by(&cmp);
-            let mut zs : Vec<f64> = coords.iter().map(|c| c.2).collect();
-            zs.sort_by(&cmp);
-
-            let range = |a: &Vec<f64>, d: Dimension| {
-                let lp = Space::dimension_idx(a[0], d).unwrap_or(0);
-                let rp = Space::dimension_idx(a[2], d).unwrap_or(11);
-                lp..rp + 1
-            };
-
-            for x in range(&xs, Dimension::X) {
-                for y in range(&ys, Dimension::Y) {
-                    for z in range(&zs, Dimension::Z) {
-                        g[x][y][z].polygons.push(p);
-                    }
-                }
-            }
-        }
-        Space { grid: g }
-    }
-
-    fn dimension_idx(a: f64, d: Dimension) -> Option<usize> {
-        let ar = match d {
-            Dimension::X => (0.0, 640.0),
-            Dimension::Y => (0.0, 480.0),
-            Dimension::Z => (0.0, 10.0),
-        };
-        if a > ar.1 || a < ar.0 {
-            None
-        } else {
-            [((a - ar.0) * 12 as f64 / (ar.1 - ar.0)).floor() as usize, 12 - 1].
-                iter().min().map(|&x| x)
-        }
-    }
-
-    fn ray_boxes(&self, origin: Vector3<f64>, direction: Vector3<f64>) ->
-    Vec<(usize, usize, usize)> {
-        let mut point = origin;
-
-        let inv_dir = Vector3(
-            1.0 / direction[Dimension::X],
-            1.0 / direction[Dimension::Y],
-            1.0 / direction[Dimension::Z]);
-
-        let dir_sign = Vector3(
-            if direction[Dimension::X] > 0.0 { 1 } else { -1 },
-            if direction[Dimension::Y] > 0.0 { 1 } else { -1 },
-            if direction[Dimension::Z] > 0.0 { 1 } else { -1 },
-        );
-
-        let dims : Vec<Dimension> = [Dimension::X, Dimension::Y, Dimension::Z].
-            iter().filter(|&&x| direction[x].abs() > f64::EPSILON).
-            map(|&x| x).collect();
-        for &d in dims.iter() {
-            if point[d] < 0.0 {
-                point = point - direction * inv_dir[d] * point[d];
-            }
-        }
-
-        let mut coords = Vector3(
-            Space::dimension_idx(point[Dimension::X], Dimension::X).
-                unwrap() as i32,
-            Space::dimension_idx(point[Dimension::Y], Dimension::Y).
-                unwrap() as i32,
-            Space::dimension_idx(point[Dimension::Z], Dimension::Z).
-                unwrap() as i32);
-
-        let nearest_bound = |c: &Vector3<i32>, d: Dimension| {
-            (c[d] + if dir_sign[d] > 0 { 1 } else { 0 }) as f64 * match d {
-                Dimension::X => 640.0,
-                Dimension::Y => 480.0,
-                Dimension::Z =>  10.0
-            } / 12.0
-        };
-
-        let mut t_max = Vector3(
-                (nearest_bound(&coords, Dimension::X) - point[Dimension::X]) * inv_dir[Dimension::X],
-                (nearest_bound(&coords, Dimension::Y) - point[Dimension::Y]) * inv_dir[Dimension::Y],
-                (nearest_bound(&coords, Dimension::Z) - point[Dimension::Z]) * inv_dir[Dimension::Z],
-            );
-
-        let coords_usize = (coords.0 as usize,
-                            coords.1 as usize,
-                            coords.2 as usize);
-
-        let mut result = vec!(coords_usize);
-        loop {
-            let mut dims_vec = dims.to_vec();
-            dims_vec.sort_by(|&a, &b| {
-                        if t_max[a] < t_max[b] {
-                            Ordering::Less
-                        } else {
-                            Ordering::Greater
-                        }});
-            let d = dims_vec[0];
-            coords[d] = coords[d] + dir_sign[d];
-            if coords[d] < 0 || coords[d] >= 12 {
-                break;
-            }
-
-            let coords_usize = (coords.0 as usize,
-                                coords.1 as usize,
-                                coords.2 as usize);
-            result.push(coords_usize);
-
-            point = point + direction * t_max[d];
-            t_max = Vector3(
-                (nearest_bound(&coords, Dimension::X) - point[Dimension::X]) * inv_dir[Dimension::X],
-                (nearest_bound(&coords, Dimension::Y) - point[Dimension::Y]) * inv_dir[Dimension::Y],
-                (nearest_bound(&coords, Dimension::Z) - point[Dimension::Z]) * inv_dir[Dimension::Z],
-            );
-        }
-
-        result
-    }
-}
-
 impl Canvas {
     pub fn new(w: u16, h: u16) -> Canvas {
         Canvas {
@@ -202,55 +52,20 @@ impl Canvas {
             Some((t, p))
         }
 
-    fn closest_polygon<'b>(origin: Vector3<f64>, direction: Vector3<f64>,
-          space: &Space<'b>) -> Option<(Polygon, Vector3<f64>, f64)> {
-              let boxes = space.ray_boxes(origin, direction);
-              for (x, y, z) in boxes {
-                  let polys = space.grid[x][y][z].polygons.clone();
-                  let mut min = f64::INFINITY;
-                  let mut poly = None;
-                  for p in polys {
-                      if let Some((t, c)) = Canvas::raytrace(
-                          origin, direction, p) {
-                              if t < min {
-                                  min = t;
-                                  poly = Some((p, c));
-                              }
+    fn closest_polygon(origin: Vector3<f64>, direction: Vector3<f64>,
+          polys: &Vec<Polygon>) -> Option<(Polygon, Vector3<f64>, f64)> {
+              let mut min = f64::INFINITY;
+              let mut poly = None;
+              for p in polys {
+                  if let Some((t, c)) = Canvas::raytrace(
+                      origin, direction, p) {
+                          if t < min {
+                              min = t;
+                              poly = Some((p, c));
                           }
-                  }
-                  if let Some((&p, c)) = poly {
-                      return Some((p, c, min));
-                  }
+                      }
               }
-              return None;
-/*                  let polys : Vec<&'b Polygon> = space.grid.iter().flat_map(
-                      |x| -> Vec<&'b Polygon> {
-                          x.iter().flat_map(
-                              |y| -> Vec<&'b Polygon> {
-                                  y.iter().flat_map(
-                                      |z| -> Vec<&'b Polygon> {
-                                          z.polygons.clone()
-                                      }).collect()
-                              }).collect()
-                      }).collect();
-
-                  let mut min = f64::INFINITY;
-                  let mut poly = None;
-                  for p in polys {
-                      if let Some((t, c)) = Canvas::raytrace(
-                          origin, direction, p) {
-                              if t < min {
-                                  min = t;
-                                  poly = Some((p, c));
-                              }
-                          }
-                  }
-                  if let Some((&p, c)) = poly {
-                      println!("Get at {:?} -> {:?}", origin, direction);
-                      return Some((p, c, min));
-                  }
-                  return None;
-                  */
+              poly.map(|(&p, c)| (p, c, min))
           }
 
     pub fn render(&mut self, scene: &World) {
@@ -258,9 +73,7 @@ impl Canvas {
                              self.height as f64 / 2.0,
                              -1.0);
         let poly = scene.shapes.iter().flat_map(|x| x.to_polygons()).collect();
-        let s = Space::new(&poly);
         for a in 0..self.width {
-            // println!("{}", a);
             for b in 0..self.height {
                 let dir : Vector3<f64> = Vector3(
                     (a as f64 - self.width  as f64 / 2.0),
@@ -268,7 +81,7 @@ impl Canvas {
                     1.0);
                 let closest = Canvas::closest_polygon(origin.into_inner(),
                                                      dir,
-                                                     &s);
+                                                     &poly);
                 if let Some((p, c, d)) = closest {
                     self.pixels[b as usize][a as usize] = p.color_at(c);
                     self.zbuffer[b as usize][a as usize] = d;
